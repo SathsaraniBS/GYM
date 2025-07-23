d0f0a507e09d95c9dd04efc372e095c69064118c
SathsaraniBS <sandunibbs@gmail.com>
2025-07-23 22:36:08 +0530

    update

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Gallery() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const gallery = ["public/images/img1.webp", "public/images/img2.webp", "public/images/img3.webp", "public/images/img4.webp", "public/images/img5.webp", "public/images/img6.webp","public/images/img7.jpg", "public/images/img8.jpg", "public/images/img9.jpg", "public/images/img10.jpg", "public/images/img11.jpg", "public/images/img12.jpg"]
+    return <section className="gallery">
+        <h1>BETER BEATS BEST</h1>
+        <div>
+            {
+                gallery.slice(0, 3).map((element, index) => (
+                    <img key={index} src={element} alt="gallery image" />
+                    )
+                    
+                )
+            }
+        </div>
+        
+        
+    </section>;
 }
 
-export default Gallery
+export default Gallery;
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
+    <section className='hero'>
+      <div className="content">
+        <div className="title">
+          <h1>LET'S</h1>
+          <h1>GET</h1>
+          <h1>MOVING</h1>
+
+        </div>
+        <div className="description">
+          <P>Your journey to fitness starts here</P>
+          <p>BSS Creation</p>
+        </div>
+
+        
+
+      </div>
       
-    </div>
+    </section>
+      
+    
   )
 }
 
