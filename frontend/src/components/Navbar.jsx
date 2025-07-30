f7f5edc14b43b7e9b35b84d39906458d0222d28d
SathsaraniBS <sandunibbs@gmail.com>
2025-07-30 22:50:08 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,28 @@ import { useState, useEffect } from 'react';
 import './Nabar.css';
 
 const Navbar = () => {
+
+  const [click, setClick] = useState(false);
+
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
+    useEffect(() => {
+        showButton();
+    }, []);
     
 
 
