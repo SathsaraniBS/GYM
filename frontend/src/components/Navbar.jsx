80a285788dc654dd070830fef4210df648997337
SathsaraniBS <sandunibbs@gmail.com>
2025-07-31 00:42:11 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Button } from './Button';
 import './Nabar.css';
 
 const Navbar = () => {
