0b233d3738d492e52f70f3d2bf7c4518d9e71a1b
SathsaraniBS <sandunibbs@gmail.com>
2025-08-01 22:47:25 +0530

    feat: add Signup page and integrate Tailwind CSS for styling
    - Created a new Signup component with a form for user registration.
    - Added input fields for Name, Email, and Password with basic validation.
    - Integrated Tailwind CSS for styling the Signup form and layout.
    - Updated package.json to include Tailwind CSS and its Vite plugin.

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-100'>
+        <div className='border shadow p-6 w-80  bg-white '>
+            <h2 className='text-2xl font-bold mb-4'>Signup</h2>
+            <form>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 '>Name</label>
+                    <input type='text' onChange={(e) => setName(e.target.value)}
+                    className='w-full px-3 py-2 border'
+                    placeholder='Enter username' required
+                     />
+                </div>
+                <div className='mb-4'>
+                    <label className='block text-gray-700'>Email</label>
+                    <input type='email' onChange={(e) => setEmail(e.target.value)} 
+                    className='w-full p-2 border ' 
+                    placeholder='Enter email' required
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label className='block text-gray-700'>Password</label>
+                    <input type='password' onChange={(e) => setPassword(e.target.value)} 
+                    className='w-full p-2 border ' 
+                    placeholder='Enter password' required/>
+                </div>
+                <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>Signup</button>
+            </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Signup
