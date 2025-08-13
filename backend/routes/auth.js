a655e2aad574dfb853ae476bba001ae3bfbf81b5
SathsaraniBS <sandunibbs@gmail.com>
2025-08-13 18:42:33 +0530

    fix: correct import statements and improve user registration logic in auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,34 +1,71 @@
-import exprees from 'express';
+import express from 'express';
 import User from '../models/User.js';
-import bcrypt from 'bcryptjs';
-const router = exprees.Router();
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-router.post('/login',async (req, res) => {
+const router = express.Router();
 
-    try {
-        const { name ,email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({
-                success: false,
-                message: "User already exists",
-            });
+router.post('/register', async(req,res) =>{
+    
+    try{
+        const {name, email , password} = req.body;
+        const user = await User.findOne({email});
+
+        if (user){
+            return res.status(401).json({success: false, message: 'User already exists'});
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-        });
-        res.status(201).json({
-            success: true,
-            user: newUser,
-        });
-        
-    } catch (error) {
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User(
+            {
+                name,
+                email,
+                password: hashPassword
+            })
+
+        await newUser.save();
+        return res.status(200).json({success: true, message: 'User registered successfully'});
+
+
+    }
+    catch(error){
+        return res.status(500).json({success: false, message: 'Error in Adding user'});
+
+    }
+
+
+})
+
+
+router.post('/login', async(req,res) =>{
+    
+    try{
+        const {email , password} = req.body;
+        const user = await User.findOne({email});
+
+        if (!user){
+            return res.status(401).json({success: false, message: 'User not exist'});
+
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword){
+            return res.status(401).json({success: false, message: ' Wrong credentials'});
+
+        }
+        const token = jwt.sign({id: user._id}, 'secretkey', {expiresIn: '1h'});
+        return res.status(200).json({success: true,token, message: 'Login successfully', token});
+
         
     }
-});
+    catch(error){
+        return res.status(500).json({success: false, message: 'Error in Login server'});
+
+    }
+
+
+})
 
 export default router;
