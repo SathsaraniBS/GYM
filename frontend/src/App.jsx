f3e963bd8b7c85991d3fe51fea30fd2cdb08d9ac
SathsaraniBS <sandunibbs@gmail.com>
2025-08-17 21:28:24 +0530

    feat: update axios version and enhance Navbar component with user authentication links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import Gallery from './components/Gallery';
 import WorkoutSessions from './components/WorkoutSessions';
@@ -11,15 +9,17 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import BMICalculater from './components/BMICalculater';
 import CustomerReview from './components/CustomerReview';
+import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css'
 import ReviewForm from './components/ReviewForm';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Router>
-        <Navbar />
+        <H />
         <Hero />
         <WorkoutSessions/>
         <Gallery />
@@ -32,7 +32,7 @@ function App() {
         
       </Router>
       
-    </div>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,16 +4,19 @@ import { useState, useEffect } from 'react';
 
 const Navbar = () => {
 
-  
-    
-    
-
 
   return (
-    <header>
-      <p>FitTrack</p>
+    <nav className='bg-gray-800 text-white flex justify-between items-center p-4'>
+      <div className='text-2xl font-bold'>
+        <Link to="/"> FitTrack</Link>
+      </div>
+      <div>
+      <span className='mr-4'>UserName</span>
+      <Link to="/login" className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout</Link>
+      <Link to="/register" className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4'>Signup</Link>
+      </div>
       
-    </header> 
+    </nav> 
     
      
   );
