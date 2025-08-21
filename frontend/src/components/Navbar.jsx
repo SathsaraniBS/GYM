7519a638f984c501487a8a37850cd962d5d8a378
SathsaraniBS <sandunibbs@gmail.com>
2025-08-21 22:32:40 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,16 @@ const Navbar = () => {
 
 
   return (
-    <nav className='bg-gray-800 text-white flex justify-between items-center p-4'>
+    <nav className='bg-black text-white flex justify-between items-center p-4'>
       <div className='text-2xl font-bold'>
         <Link to="/"> FitTrack</Link>
       </div>
       <div>
+      <Link to="/" className='mr-4 bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Home</Link>
+      <Link to="/about" className='mr-4 bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>About</Link>
+      <Link to="/pricing"className='mr-4 bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Pricing</Link>
+      <Link to="/gallery" className='mr-4 bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Gallery</Link>
+      <Link to="/contact" className='mr-4 bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Contact</Link>
       <span className='mr-4'>UserName</span>
       <Link to="/login" className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout</Link>
       <Link to="/register" className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4'>Signup</Link>
