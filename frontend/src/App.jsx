853a3e33bb4ae5c0640f89e92b113ee8e7940638
SathsaraniBS <sandunibbs@gmail.com>
2025-08-22 11:40:39 +0530

    refactor: update Signup component layout and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/register' element={<Signup />} ></Route>
+'
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,50 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-function Signup() {
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+const Signup = () => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const response = await axios.post('http://localhost:5000/api/auth/signup', {
-              name, email, password 
-            });
-          console.log(response);
-          
-        } catch (error) {
-          console.error(error);
-        }
-      };
+  const [name ,setName] = useState('');
+  const [email ,setEmail] = useState('');
+  const [password ,setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className='flex justify-center items-center h-screen bg-gray-100'>
-        <div className='border shadow p-6 w-80  bg-white '>
-            <h2 className='text-2xl font-bold mb-4'>Signup</h2>
-            <form>
-                <div className='mb-4'>
-                    <label className='block text-gray-700 '>Name</label>
-                    <input type='text' onChange={(e) => setName(e.target.value)}
-                    className='w-full px-3 py-2 border'
-                    placeholder='Enter username' required
-                     />
-                </div>
-                <div className='mb-4'>
-                    <label className='block text-gray-700'>Email</label>
-                    <input type='email' onChange={(e) => setEmail(e.target.value)} 
-                    className='w-full p-2 border ' 
-                    placeholder='Enter email' required
-                    />
-                </div>
-                <div className='mb-4'>
-                    <label className='block text-gray-700'>Password</label>
-                    <input type='password' onChange={(e) => setPassword(e.target.value)} 
-                    className='w-full p-2 border ' 
-                    placeholder='Enter password' required/>
-                </div>
-                <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>Signup</button>
-            </form>
-        </div>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className='border shadow p-6 w-80 bg-white'>
+        <h2 className='text-2xl font-bold mb-4'>Sign up</h2>
+        <form  onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <label className='block text-gray-700'>Name</label>
+            <input type="text" 
+            onChange={(e) => setName(e.target.value)}
+            className='w-full px-3 py-2 border' placeholder='Enter Username'/>
+
+          </div>
+
+          <div className='mb-4'>
+            <label className='block text-gray-700'>Email</label>
+            <input type="text" 
+            onChange={(e) => setEmail(e.target.value)}
+            className='w-full px-3 py-2 border' placeholder='Enter email'/>
+
+          </div>
+
+          <div className='mb-4'>
+            <label className='block text-gray-700'>Password</label>
+            <input type="text" 
+            onChange={(e) => setPassword(e.target.value)}
+            className='w-full px-3 py-2 border' placeholder='Enter password'/>
+
+          </div>
+          
+          <div className='mb-4'>
+          <button  type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Sign up</button>
+
+          <p className='text-center'>Already have an account? <Link to="/login" className='text-blue-500'>Login</Link></p>
+          </div>
+
+        </form>
+
+      </div>
       
     </div>
   )
