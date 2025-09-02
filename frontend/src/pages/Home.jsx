cd7b360d0450d572f7f6b74220cfe39c2e12b98e
SathsaraniBS <sandunibbs@gmail.com>
2025-09-02 11:29:16 +0530

    refactor: remove Button component and styles, update Home page to include Hero component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 function Home() {
   return (
     <div className='bg-gray-100 min-h-screen'>
       <Navbar />
+      <Hero />
     </div>
   );
 }
