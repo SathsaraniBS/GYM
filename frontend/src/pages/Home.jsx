38920e59546373e6ae204c1407292eb960be5b9a
SathsaraniBS <sandunibbs@gmail.com>
2025-09-05 20:09:39 +0530

    refactor: add missing components to Home page for improved layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import WorkoutSessions from "../components/WorkoutSessions";
+import Gallery from "../components/Gallery";
+import Pricing from "../components/Pricing";
+import Contact from "../components/Contact";
+import CustomerReview from "../components/CustomerReview";
+import ReviewForm from "../components/ReviewForm ";
+import BMICalculater from "../components/BMICalculater";
 
 function Home() {
   return (
     <div className='bg-gray-100 min-h-screen'>
       <Navbar />
       <Hero />
+      <WorkoutSessions />
+      <Gallery />
+      <Pricing />
+      <Contact />
+      <CustomerReview />
+      <ReviewForm />
+      <BMICalculater />
+      {/* <Footer /> */}
+
     </div>
   );
 }
