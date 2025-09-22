7ae2408d4c1ce9cd3c18f4fbf8feabb247ace088
SathsaraniBS <sandunibbs@gmail.com>
2025-09-22 13:24:01 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
        <Navbar /> 
         {/* <section className="h-screen relative bg-[url('/a1.jpg')] bg-black/75  bg-center bg-cover bg-no-repeat flex items-center justify-center px-4 sm:px-6 lg:px-8"></section> */}
 
-        <section className="min-h-screen relative bg-[url('/a1.jpg')] bg-black/10 bg-blend-multiply bg-center bg-cover bg-no-repeat flex items-center justify-center px-4 sm:px-6 lg:px-8"></section>
+        <section className="min-h-screen relative bg-[url('/a3.jpg')] bg-black/10 bg-blend-multiply bg-center bg-cover bg-no-repeat flex items-center justify-center px-4 sm:px-6 lg:px-8"></section>
     </div>
   )
 }
