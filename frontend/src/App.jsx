0dc16c3a7d47d8ac3358707e58ce12f2af3fbbd9
SathsaraniBS <sandunibbs@gmail.com>
2025-09-24 21:46:30 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
-import Contact from './pages/Contact'
 import About from './pages/About'
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path='/' element={<Home />} ></Route>
         <Route path='/register' element={<Signup />} ></Route>
         <Route path='/login' element={<Login />} ></Route>
-        <Route path='/contact' element={<Contact />} ></Route>
         <Route path='/about' element={<About />} ></Route>
 '
       </Routes>
