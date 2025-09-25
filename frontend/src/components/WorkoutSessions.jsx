c802d200eb728784dfaa98d58d8277a2ada441ba
SathsaraniBS <sandunibbs@gmail.com>
2025-09-25 23:02:18 +0530

    update and add images

--- frontend/src/components/WorkoutSessions.jsx
+++ frontend/src/components/WorkoutSessions.jsx
@@ -5,6 +5,7 @@ function WorkoutSessions() {
       <div className="bg-black text-white min-h-[600px] flex flex-col items-center justify-center p-6">
          <div className="flex flex-col md:flex-row justify-around w-full max-w-6xl px-4">
         <div className="mb-10 md:mb-0 md:mr-4 text-center">
+          <img src="h2.png" alt="personal training" className='mb-4 mx-auto'/>
           <h2 className="text-4xl font-bold mb-4">PERSONAL TRAINING</h2>
           <p className="mb-6">You'll look at graphs and charts in Task One, how to approach the task and the language needed for a successful answer.</p>
           <button className="bg-red-600 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
@@ -12,6 +13,7 @@ function WorkoutSessions() {
           </button>
         </div>
         <div className="text-center">
+          <img src="h3.png" alt="group training" className='mb-4 mx-auto'/>
           <h2 className="text-4xl font-bold mb-4">GROUP TRAINING</h2>
           <p className="mb-6">You'll look at graphs and charts in Task One, how to approach the task and the language needed for a successful answer.</p>
           <button className="bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
