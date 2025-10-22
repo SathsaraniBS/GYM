1148b85edf16400da3858321a2d057f6666d83c5
SathsaraniBS <sandunibbs@gmail.com>
2025-10-22 10:17:29 +0530

    update

--- frontend/src/pages/GalleryPage.jsx
+++ frontend/src/pages/GalleryPage.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Gallery from '../components/Gallery'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from 'react';
+import Gallery from '../components/Gallery';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function GalleryPage() {
   return (
-    <div>
+    <div className="bg-black text-white min-h-screen">
       <Navbar />
-        <section className="min-h-screen relative flex items-center justify-center px-4 sm:px-6 lg:px-8 pt-20 overflow-hidden">
-
+      <section className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 pt-20">
         <Gallery />
       </section>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default GalleryPage
+export default GalleryPage;
