ced38fa3f09253d189bc3bf5da93a1b7d31f9348
SathsaraniBS <sandunibbs@gmail.com>
2025-10-27 21:30:19 +0530

    feat: add Userdashboard component with Navbar integration

---
+++ frontend/src/pages/Userdashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Userdashboard() {
+  return (
+    <div className="bg-gray-100">
+      <Navbar />
+      
+
+      
+    </div>
+  )
+}
+
+export default Userdashboard
