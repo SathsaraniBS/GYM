e0b5fa99dff15cf9dae40976c21d34d888c55164
SathsaraniBS <sandunibbs@gmail.com>
2025-12-06 01:58:28 +0530

    Refactor server setup and enhance User model with additional fields

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import authRouter from './routes/auth.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+import authRouter from './routes/auth.js';
+import reviewRouter from './routes/reviews.js';
+import adminRouter from "./routes/admin.js";
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json())
 
 app.use('/api/auth',authRouter);
+app.use("/api/reviews", reviewRouter);
+app.use("/api/admin", adminRouter);
 
-app.listen(5000,()=>{
-    console.log("server is running ");
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// app.listen(5000,()=>{
+//     console.log("server is running ");
+// })
