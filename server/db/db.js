ba2dabfb0ef5cb60d7711a911348f88f8de5fd54
SathsaraniBS <sandunibbs@gmail.com>
2025-12-10 16:31:28 +0530

    Add MongoDB connection utility to db.js

---
+++ server/db/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async ()=>{
+    try{
+        await mongoose.connect("mongodb://localhost:27017/gym-app",
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000
+            }
+        );
+        console.log("Connected to MongoDB")
+
+    }catch(error){
+        console.log("Error connecting to MongoDB",error.message)
+        throw new Error("MongoDB connection failed: " + error.message)
+
+    }
+}
+
+export default connectToMongoDB;
