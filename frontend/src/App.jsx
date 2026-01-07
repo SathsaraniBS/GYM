45d754082bfe256bf5a5b8cc6311f7b95dbde87f
SathsaraniBS <sandunibbs@gmail.com>
2026-01-07 22:14:41 +0530

    Add Gym Admin Dashboard component and update routing for admin panel access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import BecomeaMember from './pages/BecomeaMember';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import UserDashboard from './pages/Userdashboard';
 import Membership from './pages/Membership';
+import GymAdminDashboard from './pages/Adminpanel';
 
 const router = createBrowserRouter(
   [
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
     // { path: '/admin/dashboard', element: <AdminDashboard /> },
     { path: '/admin', element: <AdminDashboard /> },
     { path: '/user/dashboard', element: <UserDashboard /> },
+    {path: '/adminpanel', element: <GymAdminDashboard />},
   ],
   {
     future: {
