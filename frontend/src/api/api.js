51feb6f3c860ee08ec90959d56aa81d6051c88ea
SathsaraniBS <sandunibbs@gmail.com>
2026-01-27 23:16:03 +0530

    create api.js file

---
+++ frontend/src/api/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: '/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Add a request interceptor to inject the token
+api.interceptors.request.use(
+    (config) => {
+        const userInfo = localStorage.getItem('userInfo');
+        if (userInfo) {
+            const { token } = JSON.parse(userInfo);
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
